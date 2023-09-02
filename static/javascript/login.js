2ca8b4316549fa5334f87e575ed576e536ce8738
Surribas1980 <issupl@gmail.com>
2023-09-02 18:11:50 +0000

    formularios de rexistro e entrada

---
+++ static/javascript/login.js
@@ -0,0 +1,17 @@
+logueate.addEventListener("click",()=>{
+  
+  let elemento = document.getElementById("login")
+  if(elemento.style.display == "block"){
+    elemento.style.display="none";
+  }else{
+    elemento.style.display="block";
+  }
+})
+rexistrar.addEventListener("click",()=>{
+  let elemento = document.getElementById("rexistrarme")
+  if(elemento.style.display == "block"){
+    elemento.style.display="none";
+  }else{
+    elemento.style.display="block";
+  }
+})
