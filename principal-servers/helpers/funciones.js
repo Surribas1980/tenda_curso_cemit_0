91216058193e092206c49a85a0b6ae2127b8ffa6
Surribas1980 <issupl@gmail.com>
2023-09-03 19:39:57 +0000

    Insertando usuarios

--- principal-servers/helpers/funciones.js
+++ principal-servers/helpers/funciones.js
@@ -2,6 +2,7 @@
 const express = require('express')
 const path = require('path')
 const app = express();
+const db = require('../../bd/db.js')
 /////---CONSTANTES---////
 const directorioStatico = path.join(__dirname,'../../static/views')
 
@@ -35,11 +36,61 @@ const VerPaxinaTenda = (req,res)=>{
   //res.sendFile(nomeDaPaxina,{root: <carpetaOndeEsta>})
   res.sendFile(paxina.tenda,{root: directorioStatico})
 }
+
+const BBDD_rexistroUser = (req, res,next)=>{
+  let sampleFile;
+  let uploadPath;
+  //const db = db.open();
+  const body = req.body;
+  
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return res.status(400).send('No files were uploaded.');
+  }
+  // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+  sampleFile = req.files.sampleFile;
+  uploadPath = __dirname + '/' + sampleFile.name;
+  console.log('uploadPath: ',uploadPath)
+  // Use the mv() method to place the file somewhere on your server
+  sampleFile.mv(uploadPath, function(err) {
+    if (err)
+      return res.status(500).send(err);
+
+    //res.send('File uploaded!');
+  });
+  if(req.body != undefined){
+    
+
+    db.serialize(function (){
+          db.run(`INSERT INTO usuarios (dni_cli,pwd_cli,nombre_cli,email_cli) VALUES (?,?,?,?)`,[`${body.dni}`,`${body.pwd}`,`${body.usuario}`,`${body.email}`],
+            function (error) {
+              if (error) {
+                console.error(error.message);
+              }
+              
+            }
+          );
+        
+         db.each(`SELECT * FROM usuarios`, (error, row) => {
+          if (error) {
+            throw new Error(error.message);
+          }
+          console.log(row);
+        },()=>{
+          /*res.redirect('/contacto');*/
+          
+        });//db.each
+    });//db.serialize
+    res.redirect('/contacto');
+    next();
+  }
+}
+
 const endPoints = {
   VerPaxinaContacto: '/contacto',
   VerPaxinaCursos:'/cursos',
   VerPaxinaSobreNos:'/sobrenos',
-  VerPaxinaTenda:'/tenda'
+  VerPaxinaTenda:'/tenda',
+  RexistroUser:'/rexistro'
 }
 module.exports = {
 	messageServerOn,
@@ -47,5 +98,6 @@ module.exports = {
   VerPaxinaContacto,
   VerPaxinaCursos,
   VerPaxinaSobreNos,
-  VerPaxinaTenda
+  VerPaxinaTenda,
+  BBDD_rexistroUser
   }
--- server.js
+++ server.js
@@ -26,7 +26,8 @@ const {	messageServerOn,
        VerPaxinaContacto,
        VerPaxinaCursos,
        VerPaxinaSobreNos,
-       VerPaxinaTenda
+       VerPaxinaTenda,
+       BBDD_rexistroUser
        } = require("./principal-servers/helpers/funciones")
 
 //Preparo as peticións
@@ -45,54 +46,7 @@ app.get(endPoints.VerPaxinaCursos,VerPaxinaCursos);
 app.get(endPoints.VerPaxinaSobreNos,VerPaxinaSobreNos);
 app.get(endPoints.VerPaxinaTenda,VerPaxinaTenda);
 /** Rexistro usuario */
-const db = require('./bd/db.js')
-app.post('/rexistro', function(req, res,next) {
-  let sampleFile;
-  let uploadPath;
-  //const db = db.open();
-  const body = req.body;
-  
-  if (!req.files || Object.keys(req.files).length === 0) {
-    return res.status(400).send('No files were uploaded.');
-  }
-  // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
-  sampleFile = req.files.sampleFile;
-  uploadPath = __dirname + '/' + sampleFile.name;
-  console.log('uploadPath: ',uploadPath)
-  // Use the mv() method to place the file somewhere on your server
-  sampleFile.mv(uploadPath, function(err) {
-    if (err)
-      return res.status(500).send(err);
-
-    //res.send('File uploaded!');
-  });
-  if(req.body != undefined){
-    
-
-    db.serialize(function (){
-          db.run(`INSERT INTO usuarios (dni_cli,pwd_cli,nombre_cli,email_cli) VALUES (?,?,?,?)`,[`${body.dni}`,`${body.pwd}`,`${body.usuario}`,`${body.email}`],
-            function (error) {
-              if (error) {
-                console.error(error.message);
-              }
-              
-            }
-          );
-        
-         db.each(`SELECT * FROM usuarios`, (error, row) => {
-          if (error) {
-            throw new Error(error.message);
-          }
-          console.log(row);
-        },()=>{
-          /*res.redirect('/contacto');*/
-          
-        });//db.each
-    });//db.serialize
-    res.redirect('/contacto');
-    next();
-  }
-});
+app.post(endPoints.RexistroUser,BBDD_rexistroUser);
 app.get(endPoints.VerPaxinaContacto,VerPaxinaContacto)
 //START SERVER
 app.listen(3000, messageServerOn);
